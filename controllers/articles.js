6aad204dd104da900f5adfeae83cd61762e4aa59
devangrose <devangrose@gmail.com>
2018-08-20 15:58:34 -0700

    added routes and pages

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+var db = require('../models');
 router.get('/', function (req, res){
-    res.send('hello world - articles');
+    res.render('articles/index');
+});
+router.get('/new',function (req, res){
+    res.render('articles/new');
+});
+router.get('/:id',function (req, res){
+    res.render('articles/show');
+});
+router.post('/', function (req, res){
+    res.send('posted boi');
 });
 
 module.exports = router;
--- controllers/authors.js
+++ controllers/authors.js
@@ -2,7 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res){
-    res.send('hello world - authors');
+    res.render('authors/index');
+});
+router.get('/new',function (req, res){
+    res.render('authors/new');
+});
+router.get('/:id',function (req, res){
+    res.render('authors/show');
+});
+router.post('/', function (req, res){
+    res.send('posted boi');
 });
 
 module.exports = router;
