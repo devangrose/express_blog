3c3af7f6ab1fb9518b73e244f8f98d406b44a6dd
Devan Grose <devangrose@gmail.com>
2018-08-21 11:11:46 -0700

    Added posting and showing routes

--- controllers/articles.js
+++ controllers/articles.js
@@ -2,16 +2,36 @@ var express = require('express');
 var router = express.Router();
 var db = require('../models');
 router.get('/', function (req, res){
-    res.render('articles/index');
+    db.article.findAll({
+        include: [db.author]
+    }).then(function (articles){
+        res.render('articles/index', {articles: articles});
+    });
 });
 router.get('/new',function (req, res){
-    res.render('articles/new');
+    db.author.findAll().then(function (allAuthors){
+        res.render('articles/new',{authors: allAuthors});
+    }).catch(function(err){
+        console.log(err);
+        res.send('oops');
+    });
 });
 router.get('/:id',function (req, res){
-    res.render('articles/show');
+    db.article.findOne({
+        where: {id : req.params.id },
+        include: [db.author]
+    }).then(function (foundArticle){
+        res.render('articles/show', {article: foundArticle});
+    });
 });
 router.post('/', function (req, res){
-    res.send('posted boi');
+    console.log(req.body);
+    db.article.create(req.body).then(function(createdArticle){
+        res.redirect('/articles/' + createdArticle.id);
+    }).catch(function(err){
+        console.log(err);
+        res.send('noooo');
+    });
 });
 
 module.exports = router;
--- controllers/authors.js
+++ controllers/authors.js
@@ -1,17 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
 router.get('/', function (req, res){
-    res.render('authors/index');
+    db.author.findAll().then(function(authors){
+        console.log(authors);
+        res.render('authors/index', {authors: authors});
+    });
 });
 router.get('/new',function (req, res){
     res.render('authors/new');
 });
 router.get('/:id',function (req, res){
-    res.render('authors/show');
+    db.author.findOne({
+        where: {id : req.params.id },
+        include: [db.article]
+    }).then(function (foundAuthor){
+        res.render('authors/show', {author: foundAuthor});
+    }).catch(function (err){
+        console.log(err);
+        res.send("I can't find that Author");
+    });
 });
 router.post('/', function (req, res){
-    res.send('posted boi');
+    db.author.create(req.body).then(function(createdAuthor){
+        res.redirect('/authors/' + createdAuthor.id);
+    }).catch(function(err){
+        console.log(err);
+        res.send('derp');
+    });
 });
 
 module.exports = router;
