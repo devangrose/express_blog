c7115307dd222abd6edaf542a5adf74ddb063465
Devan Grose <devangrose@gmail.com>
2018-08-21 15:56:59 -0700

    added comments

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../models');
+var async = require('async');
+
+
+
 router.get('/', function (req, res){
     db.article.findAll({
         include: [db.author]
@@ -19,7 +23,7 @@ router.get('/new',function (req, res){
 router.get('/:id',function (req, res){
     db.article.findOne({
         where: {id : req.params.id },
-        include: [db.author, db.comment]
+        include: [db.author, db.comment, db.tag]
     }).then(function (foundArticle){
         db.author.findAll().then(function(allAuthors){
             res.render('articles/show', {article: foundArticle, authors: allAuthors});
@@ -35,7 +39,29 @@ router.get('/:id',function (req, res){
 router.post('/', function (req, res){
     console.log(req.body);
     db.article.create(req.body).then(function(createdArticle){
-        res.redirect('/articles/' + createdArticle.id);
+        var tags = [];
+        if(req.body.tags){
+            tags = req.body.tags.split(',');
+        }
+        console.log(tags);
+        if(tags.length > 0){
+            async.forEach(tags, function (t, done){
+                // This code runs for each individual tag we need to add
+                db.tag.findOrCreate({
+                    where: {name: t.trim()}
+                }).spread(function (newTag, wasCreated){
+                    createdArticle.addTag(newTag).then(function (){
+                        done();
+                    });
+                });
+            },function (){
+                // This code runs when everything is done!
+                res.redirect('/articles/' + createdArticle.id);
+            });
+        }
+        else {
+            res.redirect('/articles/' + createdArticle.id);
+        }
     }).catch(function(err){
         console.log(err);
         res.send('noooo');
---
+++ models/tag.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var tag = sequelize.define('tag', {
+    name: DataTypes.STRING
+  }, {});
+  tag.associate = function(models) {
+    // associations can be defined here
+    models.tag.belongsToMany(models.article, {through: 'articleTags'});
+  };
+  return tag;
+};
